010b533a4b789d08a1521954f7bf66baf118e107
JaffarShah <sjeffshah@gmail.com>
2019-04-23 14:08:27 -0400

    remove console logs and clean up

--- test_script.js
+++ test_script.js
@@ -1,7 +1,5 @@
 const pg = require("pg");
 const settings = require("./settings"); // settings.json
-//console.log(pg);
-//console.log(settings);
 
 const client = new pg.Client({
   user     : settings.user,
@@ -11,21 +9,17 @@ const client = new pg.Client({
   port     : settings.port,
   ssl      : settings.ssl
 });
-//console.log(client);
+
 client.connect((err) => {
-  //console.log(client.connect);
   if (err) {
-    //console.log(err);
     return console.error("Connection Error", err);
   }
   client.query("SELECT $1::int AS number", ["1"], (err, result) => {
     console.log("SELECT $1::int AS number");
-    //console.log(client.query);
     if (err) {
       return console.error("error running query", err);
     }
     console.log(result.rows[0].number); //output: 1
     client.end();
-    //console.log(client.end);
   });
 });
